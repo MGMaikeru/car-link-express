68ce283d00a5984f8c64e0c2de0d40f32b2bd6c7
MGMaikeru <13miguegonza2005@gmail.com>
2025-04-08 18:39:35 -0500

    chore: add junit and sonar dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,24 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/tests'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests"],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    "^.+\\.tsx?$": "ts-jest",
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
   collectCoverage: true,
-  coverageDirectory: 'coverage',
+  coverageDirectory: "coverage",
   collectCoverageFrom: [
-    'src/domain/**/*.{ts,tsx}', // Incluir pruebas de domain
-    'src/infrastructure/**/*.{ts,tsx}', // Incluir pruebas de infrastructure
-    '!src/**/*.d.ts', // Excluir archivos de declaración
-    '!src/presentation/**', // Excluir todo lo que está en presentation
-    '!src/domain/exceptions/**', // Excluir exceptions
+    "src/domain/**/*.{ts,tsx}", // Incluir pruebas de domain
+    "src/infrastructure/**/*.{ts,tsx}", // Incluir pruebas de infrastructure
+    "!src/**/*.d.ts", // Excluir archivos de declaración
+    "!src/presentation/**", // Excluir todo lo que está en presentation
+    "!src/domain/exceptions/**", // Excluir exceptions
   ],
+  coverageReporters: ["lcov", "text"],
+  testResultsProcessor: "jest-junit",
 };
